61b48a71e9762416eb5537b77abaa4f982d8a870
ncelebic <nick@celebic.net>
2019-08-28 09:25:43 -0400

    modified setup and added license

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
-from setuptools import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='pymorpheus',
     version='0.1',
-    description='Morpheus module for Python 3',
+    description='Morpheus API wrapper for Python 3',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Nick Celebic',
     author_email='nick@celebic.net',
-    packages=['pymorpheus'],
+    license='MIT License',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
     install_requires=[
         'requests',
         'urllib3',
