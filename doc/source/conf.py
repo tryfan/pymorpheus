aa4c584261a0656388e9dddbd520f47db4f4f721
ncelebic <nick@celebic.net>
2019-09-04 12:21:44 -0400

    changed theme to RTD

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -30,7 +30,7 @@ release = '0.1.4'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'recommonmark']
+extensions = ['sphinx.ext.autodoc', 'recommonmark', 'sphinx_rtd_theme']
 autoclass_content = 'both'
 master_doc = 'index'
 
@@ -48,7 +48,7 @@ exclude_patterns = ['venv', 'setup.py']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
